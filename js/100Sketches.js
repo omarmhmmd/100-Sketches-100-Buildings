dc311f45959f84bf4fe85309d9774f322c1c52d6
omarmhmmd <omar.mhmmd@gmail.com>
2018-11-08 12:55:36 -0800

    added vue.js 001

--- js/100Sketches.js
+++ js/100Sketches.js
@@ -1,3 +1,37 @@
-$(document).bind("touchmove", function(e) {
-  e.preventDefault();
-});
+var num = Math.round(Math.random() * 100);
+new Vue({
+  el: "#main-container",
+  data: {
+    value: num,
+    images: {
+      building: 'building001.jpg'
+    },
+  },
+  methods: {
+    setImages: function() {
+      if (this.value < 10) {
+        this.value = String(this.value).padStart(2, 0);
+      }
+      if (this.value == 100) {
+        this.images.building = 'building' + this.value + '.jpg'
+      } else {
+        this.images.building = 'building0' + this.value + '.jpg'
+      }
+    }
+  },
+  beforeMount() {
+    this.setImages()
+  },
+  watch: {
+    'value': function(val, oldVal) {
+      if (val < 10) {
+        this.value = String(this.value).padStart(2, 0);
+      }
+      if (val == 100) {
+        this.images.building = 'building' + this.value + '.jpg'
+      } else {
+        this.images.building = 'building0' + this.value + '.jpg'
+      }
+    }
+  }
+})
